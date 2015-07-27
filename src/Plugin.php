a4c1fe8d8ff4b5e15df13c2b5ca0e95e83dbabf4
Barry Kooij <git@barrykooij.nl>
2015-07-27 21:38:04 +0200

    contain integration improvements

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,24 +5,65 @@ namespace Never5\WPCarManager;
 final class Plugin {
 
 	/**
+	 * @var Pimple\Container
+	 */
+	private $container = null;
+
+	/**
 	 * Constructor
 	 */
 	public function __construct() {
-		echo 'init';
 
-		// new Pimple container
-		$container = new Pimple\Container();
 
-		// add post type to pimple container
-		$container['post_type'] = function() {
-			return new PostType();
-		};
+		$this->setup_container();
+
 
 		// add post type
-		add_action( 'init', function() use( $container ) {
-			$container['post_type']->register();
+		/*
+		add_action( 'init', function() {
+			$this->container['post_type']->register();
 		});
+		*/
 
+		// register post type
+		add_action( 'init', function () {
+			PostType::register();
+		} );
+
+	}
+
+	/**
+	 * @param $key
+	 *
+	 * @return mixed
+	 */
+	public function fetch( $key ) {
+		return $this->container[ $key ];
+	}
+
+	/**
+	 * @param $key
+	 * @param $val
+	 */
+	public function attach( $key, $val ) {
+		$this->container[$key] = $val;
+	}
+
+
+	/**
+	 * Setup Pimple container
+	 */
+	private function setup_container() {
+
+		// new Pimple container
+		$this->container = new Pimple\Container();
+
+		// add post type to pimple container
+		/*
+		$this->container['post_type'] = function() {
+			return new PostType();
+		};
+		*/
 	}
 
 }
