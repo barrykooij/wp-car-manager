571f7bb5cdd0eddf29fdd947fbcdbcb099bd8445
Barry Kooij <git@barrykooij.nl>
2015-07-28 14:03:39 +0200

    post type has no archive

--- src/PostType.php
+++ src/PostType.php
@@ -47,7 +47,7 @@ class PostType {
 			'show_in_admin_bar'   => true,
 			'show_in_nav_menus'   => true,
 			'can_export'          => true,
-			'has_archive'         => true,
+			'has_archive'         => false,
 			'exclude_from_search' => true,
 			'publicly_queryable'  => true,
 			'capability_type'     => 'page',
