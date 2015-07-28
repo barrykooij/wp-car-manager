bfdb3c3b54dd293b9926eb8c55012b8e07e33ef8
Barry Kooij <git@barrykooij.nl>
2015-07-28 12:56:19 +0200

    meta box views

---
+++ assets/views/mb-car-data.php
@@ -0,0 +1,9 @@
+<?php
+if ( isset( $fields ) && count( $fields ) > 0 ) {
+	foreach ( $fields as $field ) {
+		wp_car_manager()->service( 'view_manager' )->display( 'input/' . $field['type'], array(
+			'mb_prefix' => 'wpcm-cd',
+			'field'     => $field,
+		) );
+	}
+}
