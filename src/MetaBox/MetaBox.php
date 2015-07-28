70606f67eacece4434b802124854b0cacba2e713
Barry Kooij <git@barrykooij.nl>
2015-07-28 14:32:29 +0200

    Moved all meta save security checks to base class

--- src/MetaBox/MetaBox.php
+++ src/MetaBox/MetaBox.php
@@ -46,4 +46,76 @@ abstract class MetaBox implements iMetaBox {
 			array( $this, 'meta_box_output' ), WPCarManager\PostType::PT, $this->context, $this->priority );
 	}
 
+	/**
+	 * Generate nonce key
+	 *
+	 * @return string
+	 */
+	private function get_nonce_key() {
+		return 'wpcm_nonce_' . $this->id;
+	}
+
+	/**
+	 * Generate nonce action
+	 *
+	 * @return string
+	 */
+	private function get_nonce_action() {
+		return sha1( $this->id . $this->context . $this->priority . '#never5' );
+	}
+
+	/**
+	 * Output nonce field
+	 */
+	protected function output_nonce() {
+		wp_nonce_field( $this->get_nonce_action(), $this->get_nonce_key() );
+	}
+
+	/**
+	 * check if nonce is correct
+	 *
+	 * @return bool
+	 */
+	protected function check_nonce() {
+
+		// nonce check
+		if ( isset( $_POST[$this->get_nonce_key()] ) && wp_verify_nonce( $_POST[ $this->get_nonce_key() ], $this->get_nonce_action() ) ) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Check if we should save meta data
+	 *
+	 * @param \WP_Post $post
+	 *
+	 * @return bool
+	 */
+	protected function should_save( $post ) {
+		//  check nonce
+		if ( ! $this->check_nonce() ) {
+			die('nonce false');
+			return false;
+		}
+
+		// autosave check
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return false;
+		}
+
+		// post type check
+		if ( WPCarManager\PostType::PT != $post->post_type ) {
+			return false;
+		}
+
+		// capabilities
+		if ( ! current_user_can( 'edit_post', $post->ID ) ) {
+			return false;
+		}
+
+		return true;
+	}
+
 }
