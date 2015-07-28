c1f6cfeceb31bf9720e0e502bedf70ce31c02311
Barry Kooij <git@barrykooij.nl>
2015-07-28 17:19:24 +0200

    prefix is defined in main view call

--- assets/views/mb-car-data.php
+++ assets/views/mb-car-data.php
@@ -2,7 +2,7 @@
 if ( isset( $fields ) && count( $fields ) > 0 ) {
 	foreach ( $fields as $field ) {
 		wp_car_manager()->service( 'view_manager' )->display( 'input/' . $field['type'], array(
-			'mb_prefix' => 'wpcm-cd',
+			'mb_prefix' => $mb_prefix,
 			'field'     => $field,
 		) );
 	}
