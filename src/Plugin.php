2c4e6251715f10596360d241191d302e589fc5b3
Barry Kooij <git@barrykooij.nl>
2015-07-28 12:57:08 +0200

    store version in main plugin

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,33 +4,61 @@ namespace Never5\WPCarManager;
 
 final class Plugin extends Pimple\Container {
 
+	/** @var string */
+	private $version = '1.0';
+
 	/**
 	 * Constructor
 	 *
-	 * @string main file
+	 * @param string $version
+	 * @param string $file
 	 */
-	public function __construct( $file ) {
+	public function __construct( $version, $file ) {
 
+		// set version
+		$this->version = $version;
+
+		// Pimple Container construct
+		parent::__construct();
+
+		// register file service
 		$this['file'] = function () use ( $file ) {
 			return new File( $file );
 		};
 
-		parent::__construct();
-
 		// register services early since some add-ons need 'm
 		$this->register_services();
 
-		// load rest of classes on a later hook
+		// load the plugin
 		$this->load();
 
 
 	}
 
+	/**
+	 * Get plugin version
+	 *
+	 * @return string
+	 */
+	public function get_version() {
+		return $this->version;
+	}
+
+	/**
+	 * Register services
+	 */
 	private function register_services() {
 		$provider = new PluginServiceProvider();
 		$provider->register( $this );
 	}
 
+	/**
+	 * Get service
+	 *
+	 * @param String $key
+	 *
+	 * @return mixed
+	 */
 	public function service( $key ) {
 		return $this[ $key ];
 	}
