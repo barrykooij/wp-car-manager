45865b6906968742daa81a76158b71b0a106c04d
Barry Kooij <git@barrykooij.nl>
2015-07-29 19:24:50 +0200

    VehicleFactory make method

--- src/Vehicle/VehicleFactory.php
+++ src/Vehicle/VehicleFactory.php
@@ -22,6 +22,38 @@ class VehicleFactory {
 	 * @return Vehicle
 	 */
 	public function make( $id = 0 ) {
-		return $this->repository->retrieve( $id );
+
+		// get data from repository
+		$data = $this->repository->retrieve( $id );
+
+		// for now type is always car
+		$type = ucfirst( 'car' );
+
+		// vehicle
+		$vehicle = null;
+
+		// class
+		$class = '\\Never5\WPCarManager\\Vehicle\\' . $type;
+
+		// check if class exists
+		if ( class_exists( $class ) ) {
+
+			// create vehicle type
+			/** @var Car $vehicle */
+			$vehicle = new $class;
+
+			foreach ( $data as $dkey => $dval ) {
+				$method = 'set_' . $dkey;
+				if ( method_exists( $vehicle, $method ) ) {
+					$vehicle->$method( $dval );
+				}
+			}
+
+			// set props
+			$vehicle->set_id( $data->id );
+
+		}
+
+		return $vehicle;
 	}
 }
