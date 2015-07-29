1325ab3a0a2bceb87acf5f632dc639e4c2de6f2c
Barry Kooij <git@barrykooij.nl>
2015-07-29 19:21:33 +0200

    Settings class

---
+++ src/Settings.php
@@ -0,0 +1,50 @@
+<?php
+namespace Never5\WPCarManager;
+
+class Settings {
+
+	/** @var array */
+	private $defaults;
+
+	/** @var array|null */
+	private $options = null;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		$this->set_defaults();
+		$this->load_options();
+	}
+
+	/**
+	 * Set defaults
+	 */
+	private function set_defaults() {
+		$this->defaults = array(
+			'currency'           => 'USD',
+			'currency_pos'       => 'left',
+			'decimal_separator'  => '.',
+			'thousand_separator' => ',',
+		);
+	}
+
+	/**
+	 * Load options
+	 */
+	private function load_options() {
+		$this->options = wp_parse_args( get_option( 'wpcm_settings', array() ), $this->defaults );
+	}
+
+	/**
+	 * Get option
+	 *
+	 * @param $key
+	 *
+	 * @return mixed|null
+	 */
+	public function get( $key ) {
+		return apply_filters( 'wpcm_setting_' . $key, ( isset( $this->options[ $key ] ) ? $this->options[ $key ] : null ) );
+	}
+
+}
