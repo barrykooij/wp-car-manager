9f571eb333b01756fdb6ef53600b6a80601143d8
Barry Kooij <git@barrykooij.nl>
2015-07-29 19:22:59 +0200

    added doors property to car

--- src/Vehicle/Car.php
+++ src/Vehicle/Car.php
@@ -7,6 +7,9 @@ class Car extends MotorVehicle {
 	/** @var String */
 	private $body_style;
 
+	/** @var int */
+	private $doors;
+
 	/**
 	 * @return String
 	 */
@@ -19,6 +22,20 @@ class Car extends MotorVehicle {
 	 */
 	public function set_body_style( $body_style ) {
 		$this->body_style = $body_style;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function get_doors() {
+		return $this->doors;
+	}
+
+	/**
+	 * @param int $doors
+	 */
+	public function set_doors( $doors ) {
+		$this->doors = $doors;
 	}
 
 }
