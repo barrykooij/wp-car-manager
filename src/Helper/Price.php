bef9cef0256c35d4eba51566ace13b8660d6e741
Barry Kooij <git@barrykooij.nl>
2015-07-29 19:21:56 +0200

    Price helper

---
+++ src/Helper/Price.php
@@ -0,0 +1,33 @@
+<?php
+namespace Never5\WPCarManager\Helper;
+
+abstract class Price {
+
+	/**
+	 * Get price format
+	 *
+	 * @return String
+	 */
+	public static function get_price_format() {
+		$currency_pos = get_option( 'woocommerce_currency_pos' );
+		$format = '%1$s%2$s';
+
+		switch ( $currency_pos ) {
+			case 'left' :
+				$format = '%1$s%2$s';
+				break;
+			case 'right' :
+				$format = '%2$s%1$s';
+				break;
+			case 'left_space' :
+				$format = '%1$s&nbsp;%2$s';
+				break;
+			case 'right_space' :
+				$format = '%2$s&nbsp;%1$s';
+				break;
+		}
+
+		return $format;
+	}
+
+}
