37e2ec0348b5bb455ee38c2aaf65eaf1e65f58ee
Barry Kooij <git@barrykooij.nl>
2015-07-31 00:53:11 +0200

    split vehicle data up in 2 arrays

--- templates/single-vehicle/data.php
+++ templates/single-vehicle/data.php
@@ -7,9 +7,19 @@ global $vehicle;
 ?>
 <div class="wpcm-content-block">
 	<h2><?php _e( 'Vehicle Details', 'wp-car-manager' ); ?></h2>
-	<table>
-		<?php foreach ( Never5\WPCarManager\Vehicle\Data::get_fields() as $data_key => $data_field ) : ?>
-			<?php wp_car_manager()->service( 'template_manager' )->get_template_part( 'vehicle-data/data', $data_key, array( 'key' => $data_key ) ); ?>
-		<?php endforeach; ?>
-	</table>
+	<?php
+	// get fields
+	$fields = Never5\WPCarManager\Vehicle\Data::get_fields();
+
+	// split fields into 2 arrays
+	$tables = array_chunk( $fields, ( ceil( count( $fields ) / 2 ) ) );
+	?>
+
+	<?php foreach ( $tables as $table_fields ) : ?>
+		<table>
+			<?php foreach ( $table_fields as $data_key => $data_field ) : ?>
+				<?php wp_car_manager()->service( 'template_manager' )->get_template_part( 'vehicle-data/data', $data_field['key'], array( 'key' => $data_field['key'] ) ); ?>
+			<?php endforeach; ?>
+		</table>
+	<?php endforeach; ?>
 </div>
