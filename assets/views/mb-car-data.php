d18b98d808991a69687619ef2144aeff1c010ea5
Barry Kooij <git@barrykooij.nl>
2015-07-30 17:07:12 +0200

    use new Data class

--- assets/views/mb-car-data.php
+++ assets/views/mb-car-data.php
@@ -1,9 +1,15 @@
 <?php
 if ( isset( $fields ) && count( $fields ) > 0 ) {
 	foreach ( $fields as $field ) {
+
+		// getter method for value
+		$get_method = 'get_' . $field['key'];
+
+		// load template part
 		wp_car_manager()->service( 'view_manager' )->display( 'input/' . $field['type'], array(
 			'mb_prefix' => $mb_prefix,
 			'field'     => $field,
+			'value'     => $vehicle->$get_method()
 		) );
 	}
 }
--- src/MetaBox/CarData.php
+++ src/MetaBox/CarData.php
@@ -3,86 +3,14 @@
 namespace Never5\WPCarManager\MetaBox;
 
 use Never5\WPCarManager;
+use Never5\WPCarManager\Vehicle;
 
 class CarData extends MetaBox {
-
-	private $fields = array();
 
 	/**
 	 * Constructor
 	 */
 	public function __construct() {
-
-		$this->fields = array(
-			'condition' => array(
-				'type'    => 'select',
-				'options' => array(
-					'new' => 'New',
-					'used'    => 'Used'
-				),
-				'label'   => __( 'Condition', 'wp-car-manager' ),
-				'key'     => 'condition'
-			),
-			'make'         => array(
-				'type'  => 'text',
-				'label' => __( 'Make', 'wp-car-manager' ),
-				'key'   => 'make'
-			),
-			'model'        => array(
-				'type'  => 'text',
-				'label' => __( 'Model', 'wp-car-manager' ),
-				'key'   => 'model'
-			),
-			'year'         => array(
-				'type'  => 'text',
-				'label' => __( 'Year', 'wp-car-manager' ),
-				'key'   => 'year'
-			),
-			'price'        => array(
-				'type'  => 'text',
-				'label' => __( 'Price', 'wp-car-manager' ),
-				'key'   => 'price'
-			),
-			'mileage'      => array(
-				'type'  => 'text',
-				'label' => __( 'Mileage', 'wp-car-manager' ),
-				'key'   => 'mileage'
-			),
-			'fuel_type'    => array(
-				'type'  => 'text',
-				'label' => __( 'Fuel Type', 'wp-car-manager' ),
-				'key'   => 'fuel_type'
-			),
-			'color'        => array(
-				'type'  => 'text',
-				'label' => __( 'Color', 'wp-car-manager' ),
-				'key'   => 'color'
-			),
-			'body_style'   => array(
-				'type'  => 'text',
-				'label' => __( 'Body Style', 'wp-car-manager' ),
-				'key'   => 'body_style'
-			),
-			'transmission' => array(
-				'type'    => 'select',
-				'options' => array(
-					'automatic' => 'Automatic',
-					'manual'    => 'Manual'
-				),
-				'label'   => __( 'Transmission', 'wp-car-manager' ),
-				'key'     => 'transmission'
-			),
-			'engine'       => array(
-				'type'  => 'text',
-				'label' => __( 'Engine', 'wp-car-manager' ),
-				'key'   => 'engine'
-			),
-			'doors'       => array(
-				'type'  => 'text',
-				'label' => __( 'Doors', 'wp-car-manager' ),
-				'key'   => 'doors'
-			),
-		);
 
 		parent::__construct( 'car-data', __( 'Car Data', 'wp-car-manager' ), 'side' );
 	}
@@ -98,15 +26,16 @@ class CarData extends MetaBox {
 		$this->output_nonce();
 
 		// add values to fields
-		$fields = array();
-		foreach ( $this->fields as $key => $val ) {
-			$fields[ $key ] = array_merge( $val, array( 'value' => get_post_meta( $post->ID, 'wpcm_' . $val['key'], true ) ) );
-		}
+//		$fields = Vehicle\Data::get_fields();
+//		foreach ( $fields as $key => $val ) {
+//			$fields[ $key ] = array_merge( $val, array( 'value' => get_post_meta( $post->ID, 'wpcm_' . $val['key'], true ) ) );
+//		}
 
 		// view
 		wp_car_manager()->service( 'view_manager' )->display( 'mb-car-data', array(
 			'mb_prefix' => 'wpcm-cd',
-			'fields'    => $fields
+			'fields'    => Vehicle\Data::get_fields(),
+			'vehicle'   => wp_car_manager()->service( 'vehicle_factory' )->make( $post->ID )
 		) );
 	}
 
@@ -125,8 +54,9 @@ class CarData extends MetaBox {
 
 		// save
 		if ( isset( $_POST['wpcm-cd'] ) && count( $_POST['wpcm-cd'] ) > 0 ) {
+			$fields = Vehicle\Data::get_fields();
 			foreach ( $_POST['wpcm-cd'] as $key => $val ) {
-				if ( isset( $this->fields[ $key ] ) ) {
+				if ( isset( $fields[ $key ] ) ) {
 					update_post_meta( $post->ID, 'wpcm_' . $key, $val );
 				}
 			}
