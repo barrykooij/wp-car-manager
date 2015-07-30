63ba668fb2921a74fd830a4212dd8d0b753bb8e2
Barry Kooij <git@barrykooij.nl>
2015-07-30 13:15:14 +0200

    summary data template

---
+++ templates/single-vehicle/summary-data.php
@@ -0,0 +1,18 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+
+global $vehicle;
+
+?>
+<div class="wpcm-summary-data">
+
+	<table>
+	<?php foreach ( wp_car_manager()->service( 'settings' )->get( 'summary_data' ) as $data_key ) : ?>
+		<?php wp_car_manager()->service( 'template_manager' )->get_template_part( 'vehicle-data/data', $data_key, array( 'key' => $data_key ) ); ?>
+	<?php endforeach; ?>
+	</table>
+
+</div>
