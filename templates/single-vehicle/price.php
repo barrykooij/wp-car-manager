4e577e56b9b8fc995c5cc563bf14002ce7472579
Barry Kooij <git@barrykooij.nl>
2015-07-30 13:15:26 +0200

    price template update

--- templates/single-vehicle/price.php
+++ templates/single-vehicle/price.php
@@ -7,10 +7,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $vehicle;
 ?>
 <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-
-	<p class="price"><?php echo $vehicle->get_formatted_price(); ?></p>
+	<p class="wpcm-price"><?php echo $vehicle->get_formatted_price(); ?></p>
 
 	<meta itemprop="price" content="<?php echo $vehicle->get_price(); ?>"/>
 	<meta itemprop="priceCurrency" content="<?php echo wp_car_manager()->service( 'settings' )->get( 'currency' ); ?>"/>
-
 </div>
