37723dc86cedb27d33b1e99f5628d53679e2743f
Barry Kooij <git@barrykooij.nl>
2015-07-30 17:07:27 +0200

    use new $value variable in admin views

--- assets/views/input/select.php
+++ assets/views/input/select.php
@@ -5,7 +5,7 @@
 		if ( isset( $field['options'] ) && count( $field['options'] ) > 0 ) {
 			foreach ( $field['options'] as $option_key => $option_value ) {
 				?>
-				<option value="<?php echo $option_key; ?>"<?php selected( $option_key, $field['value'] ); ?>><?php echo $option_value; ?></option>
+				<option value="<?php echo $option_key; ?>"<?php selected( $option_key, $value ); ?>><?php echo $option_value; ?></option>
 				<?php
 			}
 		}
--- assets/views/input/text.php
+++ assets/views/input/text.php
@@ -1,5 +1,5 @@
 <label for="<?php echo $field['key']; ?>">
 	<span><?php echo $field['label']; ?></span>
 	<input type="text" name="<?php echo $mb_prefix; ?>[<?php echo $field['key']; ?>]" id="<?php echo $field['key']; ?>"
-	       value="<?php echo $field['value']; ?>"/>
+	       value="<?php echo $value; ?>"/>
 </label>
