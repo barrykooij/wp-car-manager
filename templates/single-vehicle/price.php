d279262f178fa47c8a97e2c2e5d7c19c98f0340d
Barry Kooij <git@barrykooij.nl>
2015-07-30 15:48:12 +0200

    prefix price div class

--- templates/single-vehicle/price.php
+++ templates/single-vehicle/price.php
@@ -6,8 +6,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $vehicle;
 ?>
-<div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-	<p class="wpcm-price"><?php echo $vehicle->get_formatted_price(); ?></p>
+<div class="wpcm-price-wrapper" itemprop="offers" itemscope itemtype="http://schema.org/Offer">
+	<span class="wpcm-price"><?php echo $vehicle->get_formatted_price(); ?></span>
 
 	<meta itemprop="price" content="<?php echo $vehicle->get_price(); ?>"/>
 	<meta itemprop="priceCurrency" content="<?php echo wp_car_manager()->service( 'settings' )->get( 'currency' ); ?>"/>
