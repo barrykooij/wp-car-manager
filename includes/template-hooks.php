d5d34ca444833d977f44bee16d05c5ee41433a75
Barry Kooij <git@barrykooij.nl>
2015-07-30 13:17:16 +0200

    rearranged templates hooks

--- includes/template-hooks.php
+++ includes/template-hooks.php
@@ -14,12 +14,14 @@ add_action( 'wpcm_before_main_content', 'wpcm_output_content_wrapper', 10 );
 add_action( 'wpcm_after_main_content', 'wpcm_output_content_wrapper_end', 10 );
 
 /**
- * Before Single Vehicle Summary Div
+ * Vehicle Head Box
  *
+ * @see wpcm_template_single_title()
  * @see wpcm_show_vehicle_images()
  * @see wpcm_show_vehicle_thumbnails()
  */
-add_action( 'wpcm_before_single_vehicle_summary', 'wpcm_show_vehicle_images', 20 );
+add_action( 'wpcm_before_single_vehicle_head', 'wpcm_template_single_title', 5 );
+add_action( 'wpcm_before_single_vehicle_head', 'wpcm_show_vehicle_images', 20 );
 add_action( 'wpcm_vehicle_thumbnails', 'wpcm_show_vehicle_thumbnails', 20 );
 
 /**
@@ -27,12 +29,8 @@ add_action( 'wpcm_vehicle_thumbnails', 'wpcm_show_vehicle_thumbnails', 20 );
  *
  * @see wpcm_template_single_title()
  * @see wpcm_template_single_price()
- * @see wpcm_template_single_excerpt()
  * @see wpcm_template_single_meta()
  * @see wpcm_template_single_sharing()
  */
-add_action( 'wpcm_single_vehicle_summary', 'wpcm_template_single_title', 5 );
 add_action( 'wpcm_single_vehicle_summary', 'wpcm_template_single_price', 10 );
-add_action( 'wpcm_single_vehicle_summary', 'wpcm_template_single_excerpt', 20 );
-add_action( 'wpcm_single_vehicle_summary', 'wpcm_template_single_meta', 40 );
-add_action( 'wpcm_single_vehicle_summary', 'wpcm_template_single_sharing', 50 );
+add_action( 'wpcm_single_vehicle_summary', 'wpcm_template_single_summary_data', 20 );
