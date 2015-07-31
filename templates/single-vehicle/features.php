2f64a76cb7e3d15c11d04017d25d56f96251a8f5
Barry Kooij <git@barrykooij.nl>
2015-07-31 13:07:19 +0200

    features template part

---
+++ templates/single-vehicle/features.php
@@ -0,0 +1,20 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+global $vehicle;
+
+// get features
+$features = $vehicle->get_features();
+
+if ( count( $features ) > 0 ) : ?>
+	<div class="wpcm-content-block" id="wpcm-vehicle-features">
+		<h2><?php _e( 'Vehicle Features', 'wp-car-manager' ); ?></h2>
+		<ul>
+			<?php foreach ( $features as $feature ) : ?>
+				<li><?php echo $feature; ?></li>
+			<?php endforeach; ?>
+		</ul>
+	</div>
+<?php endif; ?>
