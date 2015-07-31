de91ce17d6c7c410a8ce1c0d870fd879fea119c6
Barry Kooij <git@barrykooij.nl>
2015-07-31 14:31:53 +0200

    moved meta box views to own directory

--- assets/views/meta-box/car-data.php
+++ assets/views/meta-box/car-data.php
@@ -6,7 +6,7 @@ if ( isset( $fields ) && count( $fields ) > 0 ) {
 		$get_method = 'get_' . $field['key'];
 
 		// load template part
-		wp_car_manager()->service( 'view_manager' )->display( 'input/' . $field['type'], array(
+		wp_car_manager()->service( 'view_manager' )->display( 'meta-box/input/' . $field['type'], array(
 			'mb_prefix' => $mb_prefix,
 			'field'     => $field,
 			'value'     => $vehicle->$get_method()
