d5a34833e57aedc96aea3868438ec35c377af209
Barry Kooij <git@barrykooij.nl>
2015-08-04 14:38:51 +0200

    JS code style

--- assets/js/edit-vehicle.js
+++ assets/js/edit-vehicle.js
@@ -26,9 +26,9 @@ jQuery( function ( $ ) {
     // car gallery file uploads
     var car_gallery_frame;
     var $image_gallery_ids = $( '#car_gallery' );
-    var $car_images    = $( '#car_images_container ul.car_images' );
+    var $car_images = $( '#car_images_container ul.car_images' );
 
-    jQuery( '.add_car_images' ).on( 'click', 'a', function( event ) {
+    jQuery( '.add_car_images' ).on( 'click', 'a', function ( event ) {
         var $el = $( this );
 
         event.preventDefault();
@@ -40,47 +40,47 @@ jQuery( function ( $ ) {
         }
 
         // Create the media frame.
-        car_gallery_frame = wp.media.frames.car_gallery = wp.media({
+        car_gallery_frame = wp.media.frames.car_gallery = wp.media( {
             // Set the title of the modal.
             title: $el.data( 'choose' ),
             button: {
                 text: $el.data( 'update' )
             },
             states: [
-                new wp.media.controller.Library({
+                new wp.media.controller.Library( {
                     title: $el.data( 'choose' ),
                     filterable: 'all',
                     multiple: true
-                })
+                } )
             ]
-        });
+        } );
 
         // When an image is selected, run a callback.
-        car_gallery_frame.on( 'select', function() {
+        car_gallery_frame.on( 'select', function () {
             var selection = car_gallery_frame.state().get( 'selection' );
             var attachment_ids = $image_gallery_ids.val();
 
-            selection.map( function( attachment ) {
+            selection.map( function ( attachment ) {
                 attachment = attachment.toJSON();
 
                 if ( attachment.id ) {
-                    attachment_ids   = attachment_ids ? attachment_ids + ',' + attachment.id : attachment.id;
+                    attachment_ids = attachment_ids ? attachment_ids + ',' + attachment.id : attachment.id;
                     var attachment_image = attachment.sizes.thumbnail ? attachment.sizes.thumbnail.url : attachment.url;
 
-                    $car_images.append('<li class="image" data-attachment_id="' + attachment.id + '"><img src="' + attachment_image + '" /><ul class="actions"><li><a href="#" class="delete" title="' + $el.data('delete') + '">' + $el.data('text') + '</a></li></ul></li>');
+                    $car_images.append( '<li class="image" data-attachment_id="' + attachment.id + '"><img src="' + attachment_image + '" /><ul class="actions"><li><a href="#" class="delete" title="' + $el.data( 'delete' ) + '">' + $el.data( 'text' ) + '</a></li></ul></li>' );
                 }
 
-            });
+            } );
 
             $image_gallery_ids.val( attachment_ids );
-        });
+        } );
 
         // Finally, open the modal.
         car_gallery_frame.open();
-    });
+    } );
 
     // Image ordering
-    $car_images.sortable({
+    $car_images.sortable( {
         items: 'li.image',
         cursor: 'move',
         scrollSensitivity: 40,
@@ -88,34 +88,34 @@ jQuery( function ( $ ) {
         forceHelperSize: false,
         helper: 'clone',
         opacity: 0.65,
-        start: function( event, ui ) {
+        start: function ( event, ui ) {
             ui.item.css( 'background-color', '#f6f6f6' );
         },
-        stop: function( event, ui ) {
+        stop: function ( event, ui ) {
             ui.item.removeAttr( 'style' );
         },
-        update: function() {
+        update: function () {
             var attachment_ids = '';
 
-            $( '#car_images_container ul li.image' ).css( 'cursor', 'default' ).each( function() {
+            $( '#car_images_container ul li.image' ).css( 'cursor', 'default' ).each( function () {
                 var attachment_id = jQuery( this ).attr( 'data-attachment_id' );
                 attachment_ids = attachment_ids + attachment_id + ',';
-            });
+            } );
 
             $image_gallery_ids.val( attachment_ids );
         }
-    });
+    } );
 
     // Remove images
-    $( '#car_images_container' ).on( 'click', 'a.delete', function() {
+    $( '#car_images_container' ).on( 'click', 'a.delete', function () {
         $( this ).closest( 'li.image' ).remove();
 
         var attachment_ids = '';
 
-        $( '#car_images_container ul li.image' ).css( 'cursor', 'default' ).each( function() {
+        $( '#car_images_container ul li.image' ).css( 'cursor', 'default' ).each( function () {
             var attachment_id = jQuery( this ).attr( 'data-attachment_id' );
             attachment_ids = attachment_ids + attachment_id + ',';
-        });
+        } );
 
         $image_gallery_ids.val( attachment_ids );
 
@@ -124,6 +124,6 @@ jQuery( function ( $ ) {
         $( '#tiptip_arrow' ).removeAttr( 'style' );
 
         return false;
-    });
+    } );
 
 } );
