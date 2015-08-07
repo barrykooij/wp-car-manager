0f9999d1ce8c9101287cb97963cd353912a98ff6
Barry Kooij <git@barrykooij.nl>
2015-08-07 17:06:18 +0200

    cars shortcode now adds rewrite tags

--- src/Shortcode/Cars.php
+++ src/Shortcode/Cars.php
@@ -2,13 +2,27 @@
 
 namespace Never5\WPCarManager\Shortcode;
 
+use Never5\WPCarManager\Vehicle;
+
 class Cars extends Shortcode {
 
 	/**
 	 * Constructor
 	 */
 	public function __construct() {
+
+		// parent constructor
 		parent::__construct( 'cars' );
+
+		// add rewrite tags for this shortcode
+		add_rewrite_tag('%make%', '([^&]+)');
+		add_rewrite_tag('%model%', '([^&]+)');
+		add_rewrite_tag('%price_from%', '([^&]+)');
+		add_rewrite_tag('%price_to%', '([^&]+)');
+		add_rewrite_tag('%mileage_from%', '([^&]+)');
+		add_rewrite_tag('%mileage_to%', '([^&]+)');
+		add_rewrite_tag('%frdate_from%', '([^&]+)');
+		add_rewrite_tag('%frdate_to%', '([^&]+)');
 	}
 
 	/**
@@ -20,7 +34,7 @@ class Cars extends Shortcode {
 
 		// get attributes, defaults filterable via 'wpcm_shortcode_cars_defaults' filter
 		$atts = shortcode_atts( apply_filters( 'wpcm_shortcode_' . $this->get_tag() . '_defaults', array(
-			'per_page' => 12, // @todo make this a setting later
+			'per_page' => - 1, // @todo make this a setting later
 			'orderby'  => 'date',
 			'order'    => 'DESC',
 		) ), $atts );
@@ -28,8 +42,15 @@ class Cars extends Shortcode {
 		// start output buffer
 		ob_start();
 
+//		global $wp_query;
+
+//		echo '<pre>';
+//			print_r($wp_query->query_vars);
+//		echo '</pre>';
+
+		// load template
 		wp_car_manager()->service( 'template_manager' )->get_template_part( 'archive-vehicle', '', array(
-			'atts' => $atts
+			'atts'     => $atts,
 		) );
 
 		return ob_get_clean();
