9a3ecf766a554102cad772f7966b4e72cccbd318
Barry Kooij <git@barrykooij.nl>
2015-08-08 20:07:21 +0200

    added new actions to listings-vehicle template

--- templates/listings-vehicle.php
+++ templates/listings-vehicle.php
@@ -3,9 +3,11 @@
 /**
  * wpcm_before_listings_vehicle hook
  */
-do_action( 'wpcm_before_listings_vehicle' );
+do_action( 'wpcm_before_listings' );
 ?>
 	<div class="wpcm-vehicle-listings">
+
+		<?php do_action( 'wpcm_before_listings_filter' ); ?>
 
 		<div class="wpcm-vehicle-filters">
 			<?php
@@ -16,6 +18,9 @@ do_action( 'wpcm_before_listings_vehicle' );
 			?>
 		</div>
 
+		<?php do_action( 'wpcm_after_listings_filter' ); ?>
+		<?php do_action( 'wpcm_before_listings_results' ); ?>
+
 		<div class="wpcm-vehicle-results-wrapper">
 			<?php
 			/**
@@ -24,9 +29,11 @@ do_action( 'wpcm_before_listings_vehicle' );
 			do_action( 'wpcm_listings_vehicle_results' );
 			?>
 		</div>
+
+		<?php do_action( 'wpcm_after_listings_results' ); ?>
 	</div>
 <?php
 /**
  * wpcm_after_listings_vehicle hook
  */
-do_action( 'wpcm_after_listings_vehicle' );
+do_action( 'wpcm_after_listings' );
