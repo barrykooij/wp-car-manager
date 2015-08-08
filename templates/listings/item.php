ecaf21f60267bbb723addd832a31493f430fe086
Barry Kooij <git@barrykooij.nl>
2015-08-08 13:40:12 +0200

    add price to last column list

--- templates/listings/item.php
+++ templates/listings/item.php
@@ -1,13 +1,13 @@
 <li class="wpcm-listings-item">
 	<a href="<?php echo $url; ?>" title="<?php echo $title; ?>">
-		<img src="<?php echo $img; ?>" alt="<?php echo $title; ?>" class="wpcm-listings-item-image" />
+		<?php echo $image; ?>
 		<div class="wpcm-listings-item-description">
 			<h3><?php echo $title; ?></h3>
 			<p><?php echo $description; ?></p>
 		</div>
-		<div class="wpcm-listings-item-price"><?php echo $price; ?></div>
 		<div class="wpcm-listings-item-meta">
 			<ul>
+				<li class="wpcm-price"><?php echo $price; ?></li>
 				<li><?php echo $mileage; ?></li>
 				<li><?php echo $frdate; ?></li>
 			</ul>
