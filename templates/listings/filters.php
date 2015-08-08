5a4c9d15015e59c286dc2663aee3e99c1082f8a0
Barry Kooij <git@barrykooij.nl>
2015-08-08 13:40:47 +0200

    filters template part

---
+++ templates/listings/filters.php
@@ -0,0 +1,31 @@
+<div class="wpcm-filter wpcm-filter-make">
+	<select data-placeholder="<?php _e( 'Select Make', 'wp-car-manager' ); ?>">
+		<option>Make 1</option>
+		<option>Make 2</option>
+		<option>Make 3</option>
+	</select>
+</div>
+
+<div class="wpcm-filter wpcm-filter-model">
+	<select data-placeholder="<?php _e( 'Select Model', 'wp-car-manager' ); ?>">
+		<option>Model 1</option>
+		<option>Model 2</option>
+		<option>Model 3</option>
+	</select>
+</div>
+
+<div class="wpcm-filter wpcm-filter-price">
+	<input type="text" value=""/>
+</div>
+
+<div class="wpcm-filter wpcm-filter-frdate">
+	<input type="text" value=""/>
+</div>
+
+<div class="wpcm-filter wpcm-filter-mileage">
+	<input type="text" value=""/>
+</div>
+
+<div class="wpcm-filter wpcm-filter-button">
+	<input type="submit" value="<?php _e( 'Show Results', 'wp-car-manager' ); ?>"/>
+</div>
