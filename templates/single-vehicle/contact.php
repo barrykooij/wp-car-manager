2e8cfde6339d70a1eed06df256ae9e5c6a713536
Claudio Sanches <contato@claudiosmweb.com>
2015-08-11 21:03:55 -0300

    Use antispambot() on Email Us

--- templates/single-vehicle/contact.php
+++ templates/single-vehicle/contact.php
@@ -9,7 +9,7 @@ global $vehicle;
 ?>
 <div class="wpcm-contact">
 
-	<a href="mailto:<?php echo wp_car_manager()->service( 'settings' )->get_option( 'contact_email' ); ?>?subject=<?php the_title(); ?>" class="wpcm-contact-button"><?php _e( 'Email Us', 'wp-car-manager' ); ?></a>
+	<a href="mailto:<?php echo antispambot( wp_car_manager()->service( 'settings' )->get_option( 'contact_email' ) ); ?>?subject=<?php the_title(); ?>" class="wpcm-contact-button"><?php _e( 'Email Us', 'wp-car-manager' ); ?></a>
 	<?php
 	$phone_number = wp_car_manager()->service( 'settings' )->get_option( 'contact_phone' );
 	if( '' !== $phone_number ) :
