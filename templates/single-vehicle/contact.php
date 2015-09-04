3b9015709ec23196742b7d7eb7b06da9f4f48257
Barry Kooij <git@barrykooij.nl>
2015-09-04 17:00:19 +0200

    Hide email contact button when no email address is entered

--- templates/single-vehicle/contact.php
+++ templates/single-vehicle/contact.php
@@ -9,12 +9,20 @@ global $vehicle;
 ?>
 <div class="wpcm-contact">
 
-	<a href="mailto:<?php echo antispambot( wp_car_manager()->service( 'settings' )->get_option( 'contact_email' ) ); ?>?subject=<?php the_title(); ?>" class="wpcm-contact-button"><?php _e( 'Email Us', 'wp-car-manager' ); ?></a>
+	<?php
+	$email = wp_car_manager()->service( 'settings' )->get_option( 'contact_email' );
+	if ( '' != $email ) :
+		?>
+		<a href="mailto:<?php echo antispambot( $email ); ?>?subject=<?php the_title(); ?>"
+		   class="wpcm-contact-button"><?php _e( 'Email Us', 'wp-car-manager' ); ?></a>
+	<?php endif; ?>
+
 	<?php
 	$phone_number = wp_car_manager()->service( 'settings' )->get_option( 'contact_phone' );
-	if( '' !== $phone_number ) :
-	?>
-		<a href="tel:<?php echo esc_attr( $phone_number ); ?>" class="wpcm-contact-button"><?php _e( 'Call Us', 'wp-car-manager' ); ?></a>
+	if ( '' !== $phone_number ) :
+		?>
+		<a href="tel:<?php echo esc_attr( $phone_number ); ?>"
+		   class="wpcm-contact-button"><?php _e( 'Call Us', 'wp-car-manager' ); ?></a>
 	<?php endif; ?>
 
 </div>
