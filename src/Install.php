6d750ddddf623dee3296ece17771904ad4c3d378
Barry Kooij <git@barrykooij.nl>
2015-09-07 16:48:48 +0200

    Cars listings page is now created upon installation. Fixes #36

--- src/Install.php
+++ src/Install.php
@@ -8,12 +8,29 @@ class Install {
 	 * Installation actions
 	 */
 	public static function install() {
-		// Register pot type and custom taxonomies
+
+		// register pot type and custom taxonomies
 		Vehicle\PostType::register();
 		Taxonomies::register_model_make();
 		Taxonomies::register_features();
 
-		// Flush rules after install
+		// create cars listing page if not exists
+		$listings_slug = sanitize_title( __( 'Cars', 'wp-car-manager' ) );
+		$listings_page = get_page_by_path( $listings_slug );
+
+		// check if listings page exists
+		if ( null == $listings_page ) {
+
+			// create page
+			wp_insert_post( array(
+				'post_type'    => 'page',
+				'post_title'   => __( 'Cars', 'wp-car-manager' ),
+				'post_content' => '[wpcm_cars]',
+				'post_status'  => 'publish'
+			) );
+		}
+
+		// flush rules after install
 		flush_rewrite_rules();
 	}
 }
