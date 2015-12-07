7b9f337e06616d346ceb13c39f0f32f93132c8e9
Barry Kooij <git@barrykooij.nl>
2015-12-07 12:19:26 +0100

    Start of submit-car-form template file

---
+++ templates/submit-car-form.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Car Submission Form
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+<form action="<?php echo esc_url( $action ); ?>" method="post" id="wpcm-car-form" class="wpcm-car-form" enctype="multipart/form-data">
+
+	<?php if ( apply_filters( 'wpcm_submit_car_form_show_signin', true ) ) : ?>
+
+		<?php /* load signin template file */ ?>
+
+	<?php endif; ?>
+
+	<?php if ( $can_post_listing || $can_edit_listing ) : ?>
+
+		<?php do_action( 'wpcm_submit_car_form_fields_start' ); ?>
+
+		<?php /* fields */ ?>
+
+		<?php do_action( 'wpcm_submit_car_form_fields_end' ); ?>
+
+		<p>
+			<input type="hidden" name="listing_id" value="<?php echo esc_attr( $listing_id ); ?>" />
+			<input type="submit" name="submit_car" class="wpcm-button" value="<?php esc_attr_e( $submit_button_text ); ?>" />
+		</p>
+
+	<?php else : ?>
+
+		<?php do_action( 'wpcm_submit_car_form_disabled' ); ?>
+
+	<?php endif; ?>
+</form>
