6181e3c65e0b710c5fd584bb139fa3eca152fced
Barry Kooij <git@barrykooij.nl>
2015-12-14 14:29:16 +0100

    The image template file

---
+++ templates/submit-car-form/images.php
@@ -0,0 +1,7 @@
+<h2><?php _e( 'Images', 'wp-car-manager' ); ?></h2>
+<fieldset class="wpcm-fieldset-images">
+	<div class="wpcm-form-image-upload" id="wpcm-car-submission-images">
+		<strong><?php _e( 'Click here to upload your images', 'wp-car-manager' ); ?></strong><br/>
+		<span><?php _e('You can upload multiple images at the same time, the first image will be your thumbnail.','wp-car-manager'); ?></span>
+	</div>
+</fieldset>
