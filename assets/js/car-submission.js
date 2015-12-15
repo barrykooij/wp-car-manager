58946f24feeecb0b4f973017b36ee0a559c34e2a
Barry Kooij <git@barrykooij.nl>
2015-12-15 17:28:06 +0100

    added custom events to car form submission JS to support image AJAX upload

--- assets/js/car-submission.js
+++ assets/js/car-submission.js
@@ -6,22 +6,45 @@ jQuery( function ( $ ) {
 
     // get action URL
     var $form = $( '#wpcm-car-form' );
-    var action = $form.attr( 'action' );
 
     // setup dropzone
     $( 'div#wpcm-car-submission-images' ).dropzone( {
-        url: action,
+        url: wpcm.ajax_url_post_images,
         paramName: 'wpcm_images',
         autoProcessQueue: false,
         addRemoveLinks: true,
-        acceptedFiles: 'image/*'
+        uploadMultiple: true,
+        parallelUploads: 20,
+        maxFiles: 20,
+        acceptedFiles: 'image/*',
+
+        init: function () {
+            var wcpm_dropzone = this;
+
+            // processQueue on wpcm_car_saved
+            $form.on( 'wpcm_car_saved', function ( event, response ) {
+
+                // set new URL based on SaveVehicle response
+                wcpm_dropzone.options.url = wpcm.ajax_url_post_images + '&vehicle=' + response.vehicle;
+
+                // process that queue
+                wcpm_dropzone.processQueue();
+            } );
+
+            // bind on queuecomplete
+            this.on( 'queuecomplete', function () {
+                console.log( 'queuecomplete triggerd' );
+            } );
+
+        }
     } );
-
-
-    console.log( wpcm.ajax_url_save );
 
     // catch form submission
     $form.submit( function () {
+
+        // testing
+        $form.trigger( 'wpcm_car_saved', [ { vehicle: 170 } ] );
+        return false;
 
         // args
         var args = {
@@ -30,19 +53,36 @@ jQuery( function ( $ ) {
             data: $form.serialize()
         };
 
-        // todo add spinner
+        // @todo add spinner
+        // @todo disable submit
 
         // @todo should probably change this to post instead of get
         jQuery.post( wpcm.ajax_url_save, args, function ( response ) {
 
             // check response
-            // on success process images
+            if ( response.success ) {
+
+                // @todo upload dem images
+                $form.trigger( 'wpcm_car_saved', [ response ] );
+
+            } else {
+                if ( response.errors.length > 0 ) {
+                    for ( var i = 0; i < response.errors.length; i++ ) {
+
+                        // get element
+                        var el = $( '#' + response.errors[ i ].id );
+
+                        // mark error
+                        $( el ).addClass( 'wpcm-error' );
+                        $( el ).parent().append( $( '<span>' ).addClass( 'wpcm-error-desc' ).html( response.errors[ i ].msg ) );
+
+                    }
+                }
+            }
 
             console.log( response );
 
         } );
-
-        console.log( 'Submit' );
 
         return false;
     } );
