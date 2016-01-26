841cb5911976c89031ba06a34ecdbde73f6d3e91
Barry Kooij <b.kooij@gmail.com>
2016-01-26 12:08:09 +0100

    Added page helper methods

--- src/Helper/Pages.php
+++ src/Helper/Pages.php
@@ -30,4 +30,31 @@ class Pages {
 		return $pages;
 	}
 
+	/**
+	 * Get submit page URL
+	 *
+	 * @return string
+	 */
+	public static function get_page_submit() {
+		return apply_filters( 'wpcm_page_submit', wp_car_manager()->service( 'settings' )->get_option( 'page_submit' ) );
+	}
+
+	/**
+	 * Get dashboard page URL
+	 *
+	 * @return string
+	 */
+	public static function get_page_dashboard() {
+		return apply_filters( 'wpcm_page_dashboard', wp_car_manager()->service( 'settings' )->get_option( 'page_dashboard' ) );
+	}
+
+	/**
+	 * Get listings page URL
+	 *
+	 * @return string
+	 */
+	public static function get_page_listings() {
+		return apply_filters( 'wpcm_page_listings', wp_car_manager()->service( 'settings' )->get_option( 'page_listings' ) );
+	}
+
 }
