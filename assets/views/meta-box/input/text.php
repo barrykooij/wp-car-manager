7064f725cf95eee95e4f8c99e656597a5793a239
Barry Kooij <b.kooij@gmail.com>
2016-02-04 11:20:44 +0100

    Only apply full width fields in backend to specific classes

--- assets/views/meta-box/input/text.php
+++ assets/views/meta-box/input/text.php
@@ -1,2 +1,2 @@
-<input type="text" name="<?php echo $mb_prefix; ?>[<?php echo $field['key']; ?>]" id="<?php echo $field['key']; ?>"
+<input type="text" name="<?php echo $mb_prefix; ?>[<?php echo $field['key']; ?>]" id="<?php echo $field['key']; ?>" class="wpcm-text"
        value="<?php echo $value; ?>"/>
