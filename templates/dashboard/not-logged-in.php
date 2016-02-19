fac9d37cf9154c7204c1701fa4b71054db5d2d7a
Barry Kooij <b.kooij@gmail.com>
2016-02-19 14:10:20 +0100

    added not-logged-in message to dashboard

---
+++ templates/dashboard/not-logged-in.php
@@ -0,0 +1,1 @@
+<a href="<?php echo apply_filters( 'wpcm_submit_car_form_account_login_url', wp_login_url( get_permalink() ) ); ?>"><?php _e( 'Please sign in to view your car listings', 'wp-car-manager');?></a>
