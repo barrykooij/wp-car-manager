5e68b89e7de51419d92a22542c47eb63122cb6fe
Barry Kooij <b.kooij@gmail.com>
2016-02-22 16:43:28 +0100

    Catching non existing vehicle Exception. Fixes #76

--- src/Shortcode/SubmitCarForm.php
+++ src/Shortcode/SubmitCarForm.php
@@ -32,18 +32,20 @@ class SubmitCarForm extends Shortcode {
 		ob_start();
 
 		/** @var Vehicle\Car $vehicle */
-		$vehicle = wp_car_manager()->service( 'vehicle_factory' )->make( $listing_id );
+		try{
+			$vehicle = wp_car_manager()->service( 'vehicle_factory' )->make( $listing_id );
 
-		// @todo check if we can set vehicle data based on POST data if $listing_id is 0
-
-		// load template
-		wp_car_manager()->service( 'template_manager' )->get_template_part( 'submit-car-form', '', array(
-			'vehicle'            => $vehicle,
-			'action'             => esc_url_raw( wp_unslash( $_SERVER['REQUEST_URI'] ) ),
-			'submit_button_text' => ( ( 0 != $vehicle->get_id() ) ? __( 'Save Changes', 'wp-car-manager' ) : __( 'Preview Car', 'wp-car-manager' ) ),
-			'can_post_listing'   => wp_car_manager()->service( 'user_manager' )->can_post_listing(),
-			'can_edit_listing'   => wp_car_manager()->service( 'user_manager' )->can_edit_listing( $listing_id ),
-		) );
+			// load template
+			wp_car_manager()->service( 'template_manager' )->get_template_part( 'submit-car-form', '', array(
+				'vehicle'            => $vehicle,
+				'action'             => esc_url_raw( wp_unslash( $_SERVER['REQUEST_URI'] ) ),
+				'submit_button_text' => ( ( 0 != $vehicle->get_id() ) ? __( 'Save Changes', 'wp-car-manager' ) : __( 'Preview Car', 'wp-car-manager' ) ),
+				'can_post_listing'   => wp_car_manager()->service( 'user_manager' )->can_post_listing(),
+				'can_edit_listing'   => wp_car_manager()->service( 'user_manager' )->can_edit_listing( $listing_id ),
+			) );
+		}catch( \Exception $e) {
+			wp_car_manager()->service( 'template_manager' )->get_template_part( 'submit-car-form/disabled', '' );
+		}
 
 		return ob_get_clean();
 	}
