69a5020ce9f55613e36f15c4dae2c600ae39a996
Barry Kooij <b.kooij@gmail.com>
2016-04-08 09:47:14 +0200

    Also pass current vehicle object to listings/item.php template

--- src/Ajax/GetVehicleResults.php
+++ src/Ajax/GetVehicleResults.php
@@ -73,7 +73,8 @@ class GetVehicleResults extends Ajax {
 					'description' => $vehicle->get_short_description(),
 					'price'       => $vehicle->get_formatted_price(),
 					'mileage'     => $vehicle->get_formatted_mileage(),
-					'frdate'      => $vehicle->get_formatted_frdate()
+					'frdate'      => $vehicle->get_formatted_frdate(),
+					'vehicle'     => $vehicle
 				) );
 			}
 		} else {
