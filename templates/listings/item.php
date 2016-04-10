a2cbb2709a26a58295b9c486d0503f5474a81b77
Barry Kooij <git@barrykooij.nl>
2016-04-10 23:35:07 +0200

    Added wpcm_vehicle_listings_item_start and wpcm_vehicle_listings_item_end actions

--- templates/listings/item.php
+++ templates/listings/item.php
@@ -1,5 +1,6 @@
 <li class="wpcm-listings-item">
 	<a href="<?php echo $url; ?>" title="<?php echo $title; ?>">
+		<?php do_action( 'wpcm_vehicle_listings_item_start', $vehicle ); ?>
 		<?php echo $image; ?>
 		<div class="wpcm-listings-item-description">
 			<h3><?php echo $title; ?></h3>
@@ -13,5 +14,6 @@
 				<li><?php echo $frdate; ?></li>
 			</ul>
 		</div>
+		<?php do_action( 'wpcm_vehicle_listings_item_end', $vehicle ); ?>
 	</a>
 </li>
