6b5b6336aae93f1ef6639868d8ca4760c2408b33
JeroenSormani <jeroen.sormani@gmail.com>
2016-04-10 18:17:40 +0200

    Settings page improvement - when refreshing the page, stay on the same tab

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -4,7 +4,13 @@ jQuery( function ( $ ) {
         $( '.nav-tab-active' ).removeClass( 'nav-tab-active' );
         $( $( this ).attr( 'href' ) ).show();
         $( this ).addClass( 'nav-tab-active' );
+
+        // Set url parameter
+        var href = window.location.href;
+        href = href.replace( /\&tab=[a-zA-Z0-9-_]+/g, '' );
+        href = href + '&tab=' + $( this ).attr( 'href' ).replace( /\#settings-/, '' );
+        window.history.replaceState( 'Object', 'Title', href );
+        
         return false;
     } );
-    $( '.nav-tab-wrapper a:first' ).click();
 } );
